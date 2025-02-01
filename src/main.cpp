7dd73ec97795d6f69d14a1b7e1a03de0f962365c
Boop <102631339+BoopEnthusiast@users.noreply.github.com>
2025-02-02 11:55:03 +1300

    comments

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,17 @@
 #include "raymath.h"
 #include <iostream>
 #include <vector>
-#include <unordered_map>
 #include <unordered_set>
 
 
 void UpdateDrawFrame();
 
-struct Player 
+struct Entity
+{
+    virtual void init() = 0;
+};
+
+struct Player : Entity
 {
     const float SPEED = 200.0f;
     const float RADIUS = 40.0f;
@@ -16,12 +20,23 @@ struct Player
     Vector2 position;
     Vector2 velocity;
 
-    void update(float delta) 
-    {   
+    void init() override
+    {
+
+    }
+
+    void update(float delta)
+    {
+        // Get direction of movement
         Vector2 direction{};
         direction.x = (IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D)) - (IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A));
         direction.y = (IsKeyDown(KEY_DOWN)  || IsKeyDown(KEY_S)) - (IsKeyDown(KEY_UP)   || IsKeyDown(KEY_W));
 
+        // Steps happening here:
+        // Normalize direction
+        // Multiply direction by speed to make velocity
+        // Multiply velocity by delta
+        // Add velocity to position
         position = Vector2Add(position, Vector2Scale(Vector2Multiply(Vector2Normalize(direction), (Vector2) {SPEED, SPEED}), delta));
     }
 } player;
@@ -29,11 +44,12 @@ struct Player
 const int screenWidth = 850;
 const int screenHeight = 500;
 
-int main() 
+
+int main()
 {
     InitWindow(screenWidth, screenHeight, "Raylib Test");
 
-    while (!WindowShouldClose()) 
+    while (!WindowShouldClose())
     {
         UpdateDrawFrame();
     }
@@ -44,19 +60,23 @@ int main()
 
 
 // Update and draw game frame
-void UpdateDrawFrame() 
-{   
+void UpdateDrawFrame()
+{
+    // Update player
     player.update(GetFrameTime());
 
+
+    /* Draw phase */
     BeginDrawing();
-
+    // Set background/clear previous frame
     ClearBackground(RAYWHITE);
-
-    DrawText("Welcome to raylib basic sample", 10, 40, 20, DARKGRAY);
-
-    DrawFPS(10, 10);
-
+    
+    // Draw player
     DrawCircle(player.position.x, player.position.y, player.RADIUS, RED);
 
+    // Show FPS
+    DrawFPS(10, 10);
+
     EndDrawing();
+    /* End draw phase */
 }
